30a3f55add4028d90e2241e70e30f17a79c03114
szyma11166-ship-it <szyma11166@gmail.com>
2026-04-16 11:09:41 +0200

    Create notifier.py

---
+++ app/notifier.py
@@ -0,0 +1,13 @@
+
+import requests
+from app.config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+def send_telegram_message(text: str):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": text,
+        "parse_mode": "Markdown"
+    }
+    response = requests.post(url, json=payload)
+    response.raise_for_status()
